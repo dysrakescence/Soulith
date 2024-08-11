ff8a6aa85d10284ba665e2ed9930c6f741d70a6f
dysrakescence <admin@dysrakescence.eu.org>
2024-08-10 23:13:49 -0400

    Implement simple in/out echo program

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,17 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var buf: [32]u8 = undefined;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    try stdout.print("> ", .{});
+    while (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
+        if (std.mem.eql(u8, user_input, "")) {
+            try stdout.print("goodbye!\n", .{});
+            break;
+        }
+        try stdout.print("{s}\n> ", .{user_input});
+    }
 }
